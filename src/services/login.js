cd2b8e06c8a36765190095813bcbf2f8e369ff14
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 17:32:16 -0300

    feat: imports dotenv and declare JWT_SECRET

--- src/services/login.js
+++ src/services/login.js
@@ -1,5 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { User } = require('../database/models');
+require('dotenv').config();
+
+const { JWT_SECRET } = process.env;
 
 const loginService = {
   userLogin: (userData) => {
