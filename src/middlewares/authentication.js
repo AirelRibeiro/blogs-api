979afd4392f48bb392fb4025931000b1d75a4fca
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 19:04:53 -0300

    feat: includes Try for decodification of token

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,3 +1,6 @@
 require('dotenv').config();
   const { authorization } = req.headers;
   if (!authorization) return next({ message: 'Token not found', code: 401 });
+    const decodedInformation = jwt.verify(authorization, JWT_SECRET);
+    req.userData = decodedInformation;
+    return next();
