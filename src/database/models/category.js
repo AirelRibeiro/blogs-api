5065c11de8d079b91f3eb70316e91297b8054cec
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 15:52:29 -0300

    feat: implements Category model with sequelize.define

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+const Category =  (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  Category.associate = (models) => {};
+
+  return Category;
+}
+
+module.exports = Category;
