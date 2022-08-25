cda287deb38858b90beda1a42ef2286ee4c92aca
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 16:10:42 -0300

    feat: implements BlogPost model with sequelize.define

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,18 @@
+const BlogPost =  (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    timestamps: false,
+  });
+
+
+  return BlogPost;
+}
+
+module.exports = BlogPost;
