e6ad55c4b9bcb4083657eb5863f4bd35504233ca
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 18:32:54 -0300

    feat: imports and includes loginRoute in App

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
-// ...
+const loginRoute = require('./routes/login');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('login', loginRoute);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
