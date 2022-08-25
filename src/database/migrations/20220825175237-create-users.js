898635d2776fc6a4e1bf68b5f329cabca20452eb
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 15:33:41 -0300

    feat: implements up and down of create-users migration

--- src/database/migrations/20220825175237-create-users.js
+++ src/database/migrations/20220825175237-create-users.js
@@ -2,20 +2,32 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface) => queryInterface.dropTable('Users'),
 };
