50343d770e9a66c9918044f78ac8ad9e297e3123
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 15:48:27 -0300

    feat: implements User model with sequelize.define

---
+++ src/database/models/users.js
@@ -0,0 +1,18 @@
+const User =  (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {};
+
+  return User;
+}
+
+module.exports = User;
