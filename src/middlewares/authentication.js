79b9c5f16d24084959cfb416094bfabe27345e8b
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 19:20:25 -0300

    feat: exports tokenAuthentication

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
+
+const tokenAuthentication = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) return next({ message: 'Token not found', code: 401 });
+  try {
     const decodedInformation = jwt.verify(authorization, JWT_SECRET);
     req.userData = decodedInformation;
     return next();
   } catch (err) {
     return next({ message: 'Expired or invalid token', code: 401 });
   }
+};
+
+module.exports = tokenAuthentication;
