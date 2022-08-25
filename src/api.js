43e5c1b12e1c670cfa0e4c2288ffa903c347541f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 18:45:10 -0300

    feat: includes errorMiddleware in App

--- src/api.js
+++ src/api.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/error');
 const loginRoute = require('./routes/login');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('login', loginRoute);
+app.use('/login', loginRoute);
+
+app.use(errorMiddleware);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
