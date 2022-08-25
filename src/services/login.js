c2278e3870ebb92f1cd60bec79b77fac75db9bfe
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 17:55:16 -0300

    feat: implements user checking

--- src/services/login.js
+++ src/services/login.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 const { JWT_SECRET } = process.env;
 
 const loginService = {
-  userLogin: (userData) => {
-
+  userLogin: async ({ email, password }) => {
+    const user = await User.findOne({ where: { email } });
+    if (!user) return { message: 'Invalid fields', code: 400 };
+    if (user && user.password !== password) return { message: 'Invalid fields', code: 400 };
   },
 };
+
+module.exports = loginService;
