10954d97d08a09da5fee130618ef88aabea11734
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 19:44:00 -0300

    feat: includes id in payload token

--- src/services/login.js
+++ src/services/login.js
@@ -10,7 +10,8 @@ const loginService = {
     if (!user) return { message: 'Invalid fields', code: 400 };
     if (user && user.password !== password) return { message: 'Invalid fields', code: 400 };
 
-    const token = jwt.sign({ data: email }, JWT_SECRET, { algorithm: 'HS256' });
+    const token = jwt
+      .sign({ data: { email, id: user.dataValues.id } }, JWT_SECRET, { algorithm: 'HS256' });
     return { token };
   },
 };
