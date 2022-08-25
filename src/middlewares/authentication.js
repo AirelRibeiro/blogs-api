9eef3dfca8c1bf5753c7c4967a8c327779f410f6
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 19:47:04 -0300

    refactor: specifies part of the decoded message to be used in the req.userData

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -3,12 +3,12 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
 
-const tokenAuthentication = (req, res, next) => {
+const tokenAuthentication = (req, _res, next) => {
   const { authorization } = req.headers;
   if (!authorization) return next({ message: 'Token not found', code: 401 });
   try {
     const decodedInformation = jwt.verify(authorization, JWT_SECRET);
-    req.userData = decodedInformation;
+    req.userData = decodedInformation.data;
     return next();
   } catch (err) {
     return next({ message: 'Expired or invalid token', code: 401 });
