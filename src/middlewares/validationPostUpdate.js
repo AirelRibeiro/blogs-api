bc0e27a95e037956167bb2f9f46b9ae0ec30f98e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 17:25:37 -0300

    feat: creates fields validation

--- src/middlewares/validationPostUpdate.js
+++ src/middlewares/validationPostUpdate.js
@@ -1,3 +1,9 @@
+const validateFieldsExistence = (body) => {
+  if (!body.title || !body.content) {
+    return { message: 'Some required fields are missing', code: 400 };
+  }
+  return true;
+};
 const validatePost = async (postId) => {
   const post = await blogPostService.findOnePost(postId);
   return post;
