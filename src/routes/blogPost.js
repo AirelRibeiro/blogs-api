71d36ef929d8c8eec5b12cd42350c51792674537
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 17:27:15 -0300

    feat: includes route for update in blogPostRoute

--- src/routes/blogPost.js
+++ src/routes/blogPost.js
@@ -2,6 +2,7 @@ const express = require('express');
 const blogPostController = require('../controllers/blogPost');
 const tokenAuthentication = require('../middlewares/authentication');
 const validPost = require('../middlewares/validationPostCreate');
+const validUpdate = require('../middlewares/validationPostUpdate');
 
 const blogPostRoute = express.Router();
 
@@ -10,5 +11,6 @@ blogPostRoute.use(tokenAuthentication);
 blogPostRoute.get('/:id', blogPostController.findOnePost);
 blogPostRoute.get('/', blogPostController.findAll);
 blogPostRoute.post('/', validPost, blogPostController.create);
+blogPostRoute.put('/:id', validUpdate, blogPostController.update);
 
 module.exports = blogPostRoute;
