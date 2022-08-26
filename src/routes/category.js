6bd3f7a22b8c6a88a5e980a137b6144205c1f178
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 23:05:31 -0300

    feat: includes route for category creation in database

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const categoryController = require('../controllers/category');
 const tokenAuthentication = require('../middlewares/authentication');
 const nameValidate = require('../middlewares/validationCategory');
+
 const categoryRoute = express.Router();
 
 categoryRoute.use(tokenAuthentication);
+
+categoryRoute.post('/', nameValidate, categoryController.create);
+
 module.exports = categoryRoute;
