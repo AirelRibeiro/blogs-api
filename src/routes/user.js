563b6d6ec44a0d255dc87e0104a2af1dc96495b2
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 21:38:09 -0300

    feat: imports tokenAuthentication in userRoute

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/user');
+const tokenAuthentication = require('../middlewares/authentication');
 const userFields = require('../middlewares/validationUser');
 
 const userRoute = express.Router();
