fb7623752994803e13b1e52cf58e6eb04a31da41
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 16:44:54 -0300

    fix: includes default value for published and updated

--- src/database/models/blogPost.js
+++ src/database/models/blogPost.js
@@ -4,8 +4,8 @@ const BlogPost =  (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
-    published: DataTypes.DATE,
-    updated: DataTypes.DATE,
+    published: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updated: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
   },
   {
     timestamps: false,
