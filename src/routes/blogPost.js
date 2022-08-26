2ac0fb00311f645fa95b8449d2cba124ae3448b7
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 12:08:24 -0300

    feat: includes route for get all posts in blogPostRoute

--- src/routes/blogPost.js
+++ src/routes/blogPost.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const blogPostController = require('../controllers/blogPost');
 const tokenAuthentication = require('../middlewares/authentication');
 
 const blogPostRoute = express.Router();
 
 blogPostRoute.use(tokenAuthentication);
+blogPostRoute.get('/', blogPostController.findAll);
+
 module.exports = blogPostRoute;
