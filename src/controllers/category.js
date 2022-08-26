aefd80cd073b60c6f071e32eeb4028be815e79b1
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 22:57:59 -0300

    feat: creates normal flow for create new category database

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,3 +1,7 @@
 const categoryController = {
-  create: 
+  create: async (req, res, next) => {
+    try {
+      const category = await categoryService.create(req.body.name);
+      return res.status(201).json(category);
+    } catch (err) {
 };
