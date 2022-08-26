554d6c19b57a34683ab024725a90eda7ebf5c751
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 12:07:39 -0300

    feat: includes tokenAuthentication in blogPostsRoute

--- src/routes/blogPost.js
+++ src/routes/blogPost.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const tokenAuthentication = require('../middlewares/authentication');
+
 const blogPostRoute = express.Router();
 
+blogPostRoute.use(tokenAuthentication);
 module.exports = blogPostRoute;
