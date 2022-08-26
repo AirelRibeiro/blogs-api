5381aa5e6cdadc5dfe6583100dd5c89cfce3b249
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 18:11:00 -0300

    feat: creates middleware of deletion validation

--- src/middlewares/validationPostDelete.js
+++ src/middlewares/validationPostDelete.js
@@ -1,5 +1,18 @@
+const blogPostService = require('../services/blogPost');
 
 const validatePost = async (postId) => {
   const post = await blogPostService.findOnePost(postId);
   return post;
 };
+
+const validDelete = async (req, res, next) => {
+  const validPost = await validatePost(req.params.id);
+  if (validPost.message) return next(validPost);
+
+  const { id } = req.userData;
+  if (id !== validPost.userId) return next({ message: 'Unauthorized user', code: 401 });
+
+  next();
+};
+
+module.exports = validDelete;
