c552a484e0f04174d469701296bed48c72a01ba8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 12:10:34 -0300

    feat: creates findAll for search and list all posts in database

--- src/services/blogPost.js
+++ src/services/blogPost.js
@@ -1,3 +1,12 @@
 const blogPostService = {
+  findAll: async () => {
+    const posts = await BlogPost.findAll({
+      include: [
+        { model: User, as: 'user', attributes: { exclude: ['password'] } },
+        { model: Category, as: 'categories', through: { attributes: [] } },
+      ],
+    });
+    return posts;
+  },
 
 module.exports = blogPostService;
