fec449082d25ce160f396687d71ad29e7aa6d708
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 17:26:16 -0300

    feat: creates middleware for post update validation

--- src/middlewares/validationPostUpdate.js
+++ src/middlewares/validationPostUpdate.js
@@ -1,11 +1,29 @@
+const blogPostService = require('../services/blogPost');
+
 const validateFieldsExistence = (body) => {
   if (!body.title || !body.content) {
     return { message: 'Some required fields are missing', code: 400 };
   }
   return true;
 };
+
 const validatePost = async (postId) => {
   const post = await blogPostService.findOnePost(postId);
   return post;
 };
 
+const validUpdate = async (req, res, next) => {
+  const { body } = req;
+  const validate = validateFieldsExistence(body);
+  if (validate.message) return next(validate);
+
+  const validPost = await validatePost(req.params.id);
+  if (validPost.message) return next(validPost);
+
+  const { id } = req.userData;
+  if (id !== validPost.userId) return next({ message: 'Unauthorized user', code: 401 });
+
+  next();
+};
+
+module.exports = validUpdate;
