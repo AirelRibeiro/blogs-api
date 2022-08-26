07d5e25c644a250ccf604ff814de9b0c3ee4c2b1
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-26 11:51:11 -0300

    feat: implements nomal flow in findoAll for search and returns all posts

--- src/controllers/blogPost.js
+++ src/controllers/blogPost.js
@@ -2,6 +2,10 @@ const blogPostService = require('../services/blogPost');
 
 const blogPostController = {
   findAll: async (req, res, next) => {
+    try {
+      const posts = await blogPostService.findAll();
+      return res.status(200).json(posts);
+    } catch (err) {
   },
 };
 module.exports = blogPostController;
