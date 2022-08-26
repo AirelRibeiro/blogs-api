354525e2c01b22e4b761ec56304c70659e1ab749
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 22:59:24 -0300

    feat: creates error tratamet if category criation fail

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -4,4 +4,6 @@ const categoryController = {
       const category = await categoryService.create(req.body.name);
       return res.status(201).json(category);
     } catch (err) {
+      return next(err);
+    }
 };
