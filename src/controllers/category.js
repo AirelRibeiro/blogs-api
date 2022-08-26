005f169066a80ba4c1234b3b4e36222cc8da9278
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 23:16:33 -0300

    feat: creates findAll in categoryController for list all categories

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -9,6 +9,15 @@ const categoryController = {
       return next(err);
     }
   },
+
+  findAll: async (_req, res, next) => {
+    try {
+      const categories = await categoryService.findAll();
+      return res.status(200).json(categories);
+    } catch (err) {
+      return next(err);
+    }
+  },
 };
 
 module.exports = categoryController;
