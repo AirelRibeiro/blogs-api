23ba1090dc9ed786da6128ba00c5d84f7a2eddc8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-08-25 23:02:50 -0300

    feat: includes token authentication in categoryRoute

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const tokenAuthentication = require('../middlewares/authentication');
 const categoryRoute = express.Router();
 
+categoryRoute.use(tokenAuthentication);
 module.exports = categoryRoute;
